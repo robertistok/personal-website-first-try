585d3bee6730079f0c8e49c4ed7836aa89cb3d13
robertistok <robertistok94@gmail.com>
2019-07-19 13:07:19 +0200

    Layout to TS

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,4 +1,3 @@
-// @flow
 import React from "react";
 import renderer from "react-test-renderer";
 import Layout from "./Layout";
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,14 @@
-// @flow
 import React from "react";
 import Helmet from "react-helmet";
-import type { Node as ReactNode } from "react";
 import styles from "./Layout.module.scss";
 
-type Props = {
-  children: ReactNode,
-  title: string,
-  description?: string
-};
+interface LayoutProps {
+  children: React.ReactNode;
+  title: string;
+  description?: string;
+}
 
-const Layout = ({ children, title, description }: Props) => (
+const Layout: React.FunctionComponent<LayoutProps> = ({ children, title, description }) => (
   <div className={styles.layout}>
     <Helmet>
       <html lang="en" />
