42d8782d1165fbd0915c39da149ca5f6a3a7872c
robertistok <robertistok94@gmail.com>
2019-07-23 21:40:27 +0200

    Add enzyme and set up jest ot run tsx

--- jest/jest-config.js
+++ jest/jest-config.js
@@ -1,19 +1,25 @@
-"use strict";
-
 module.exports = {
+  preset: "ts-jest",
   rootDir: "../",
   transform: {
+    "\\.(ts|tsx)$": "ts-jest",
     "^.+\\.js?$": "<rootDir>/jest/jest-preprocess.js"
   },
-  testMatch: ["**/__tests__/**/*.js", "**/?(*.)test.js"],
+  testMatch: [
+    "**/?(*.)+(spec|test).ts?(x)",
+    "**/?(*.)+(spec|test).js?(x)",
+    "**/__tests__/**/*.ts?(x)",
+    "**/__tests__/**/*.js?(x)"
+  ],
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest/__mocks__/file-mock.js"
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/jest/__mocks__/file-mock.js"
   },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testPathIgnorePatterns: ["node_modules", ".cache", "public"],
-  transformIgnorePatterns: [
-    "node_modules/(?!(gatsby)/)"
-  ],
+  setupFilesAfterEnv: ["<rootDir>/jest/jest.setup.js"],
+  transformIgnorePatterns: ["<rootDir>/node_modules/", "node_modules/(?!(gatsby)/)"],
   globals: {
     __PATH_PREFIX__: ""
   },
